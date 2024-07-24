f7c3fe6095703fd398b29de1b3fed2c61f87f0a1
tamarsh224@gmail.com <tamarsh224@gmail.com>
2024-07-24 18:48:27 +0300

    added tests file

---
+++ tests.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+import requests
+
+
+def test_reachability():
+    for i in range(3):
+        time.sleep(2)
+        assert requests.get("http://127.0.0.1:8000/").status_code == 200
